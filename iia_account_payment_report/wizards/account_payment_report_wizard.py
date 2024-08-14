e5f5205b88487f9284fbf9ce551a8cc61ab5530a
alyra0600 <alyra.sistemas@gmail.com>
2024-08-14 17:04:53 -0600

    campo memo reporte de pagos

--- iia_account_payment_report/wizards/account_payment_report_wizard.py
+++ iia_account_payment_report/wizards/account_payment_report_wizard.py
@@ -89,6 +89,7 @@ class AccountPaymentReportWizard(models.TransientModel):
                 am_credit.id AS payment_id,
                 am_debit.invoice_date,
                 apr.max_date AS payment_date,
+                am.debit.ref as memo_pago,
                 am_debit.l10n_mx_edi_cfdi_uuid as folio_factura,
                 am_credit.l10n_mx_edi_cfdi_uuid as folio_pago,
                 am_credit.journal_id,
