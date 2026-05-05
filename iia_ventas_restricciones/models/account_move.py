5ceeace5d98893d3692625cd32cbec84297ab8ad
integraqlikview <integraqlikview>
2026-05-05 14:48:44 -0600

    fix

--- iia_ventas_restricciones/models/account_move.py
+++ iia_ventas_restricciones/models/account_move.py
@@ -14,15 +14,17 @@ class AccountMove(models.Model):
         return super().action_reverse()
 
     def button_cancel(self):
-        if not self.env.user.has_group('iia_ventas_restricciones.group_can_cancel_invoice'):
-            raise AccessError(_('No tiene permiso para cancelar facturas.'))
-        if not self.env.context.get('from_cancel_wizard'):
-            return {
-                'name': _('Motivo de Cancelación'),
-                'type': 'ir.actions.act_window',
-                'res_model': 'cancel.invoice.wizard',
-                'view_mode': 'form',
-                'target': 'new',
-                'context': {'default_invoice_ids': [(6, 0, self.ids)]},
-            }
+        invoice_types = ('out_invoice', 'out_refund', 'in_invoice', 'in_refund', 'out_receipt', 'in_receipt')
+        if any(move.move_type in invoice_types for move in self):
+            if not self.env.user.has_group('iia_ventas_restricciones.group_can_cancel_invoice'):
+                raise AccessError(_('No tiene permiso para cancelar facturas.'))
+            if not self.env.context.get('from_cancel_wizard'):
+                return {
+                    'name': _('Motivo de Cancelación'),
+                    'type': 'ir.actions.act_window',
+                    'res_model': 'cancel.invoice.wizard',
+                    'view_mode': 'form',
+                    'target': 'new',
+                    'context': {'default_invoice_ids': [(6, 0, self.ids)]},
+                }
         return super().button_cancel()
